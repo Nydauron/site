8ffe59759d6516348483c9e823423588e58f0ca7
Jareth Gomes <jarethgomes@gmail.com>
2024-01-31 21:32:39 +0000

    Added home page and project page
    Updated astro to v4 and other dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import tailwindcssNesting from "tailwindcss/nesting";
+import icon from "astro-icon";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon(), mdx()],
+  vite: {
+    css: {
+      postcss: {
+        plugins: [tailwindcssNesting()],
+      },
+    },
+  },
 });
